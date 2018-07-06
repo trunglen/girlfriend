9a75620930cc3d4715c03e040756eea72d2e5a05
trunglen <trunglenlvn@gmail.com>
2018-07-06 15:30:26 +0700

    sda

---
+++ app/src/main/java/chat/girlfriend/girlfriendchat/ui/GirlInfoActivity.kt
@@ -0,0 +1,12 @@
+package chat.girlfriend.girlfriendchat.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class GirlInfoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_girl_info)
+    }
+}
---
+++ app/src/main/java/chat/girlfriend/girlfriendchat/ui/VideoGirl.kt
@@ -0,0 +1,12 @@
+package chat.girlfriend.girlfriendchat.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class VideoGirl : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video_girl)
+    }
+}
