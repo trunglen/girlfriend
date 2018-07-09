22b440863d3605dab24b1d99a2423ac095c3f14f
trunglen <trunglenlvn@gmail.com>
2018-07-10 02:11:37 +0700

    cxc

--- app/src/main/java/chat/girlfriend/girlfriendchat/MainActivity.kt
+++ app/src/main/java/chat/girlfriend/girlfriendchat/MainActivity.kt
@@ -1,19 +1,16 @@
 package chat.girlfriend.girlfriendchat
 
+import android.app.Activity
 import android.content.Intent
 import android.support.design.widget.TabLayout
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
-import android.support.v4.view.ViewPager
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
@@ -23,6 +20,8 @@ import chat.girlfriend.girlfriendchat.utils.removeLocalStorage
 import com.google.firebase.database.*
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_favourite.*
+import kotlinx.android.synthetic.main.fragment_hot.*
 import kotlinx.android.synthetic.main.fragment_main.*
 
 
@@ -54,97 +53,52 @@ class MainActivity : AppCompatActivity() {
         override fun getItem(position: Int): Fragment {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1)
+            if (position == 0)
+//                return PlaceholderFragment.newInstance(position + 1)
+                return NewFragment()
+            else if (position == 1)
+                return HotFragment()
+            else
+                return FavouriteFragment()
         }
 
         override fun getCount(): Int {
             // Show 3 total pages.
             return 3
         }
+
     }
 
     /**
      * A placeholder fragment containing a simple view.
      */
-    class PlaceholderFragment : Fragment() {
+    class NewFragment : Fragment() {
         // Write a message to the database
         var database = FirebaseDatabase.getInstance()
         var myRef = database.getReference("girl")
         var girls = ArrayList<Girl>()
-        var firstFavourite = false
         override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                                   savedInstanceState: Bundle?): View? {
             val rootView = inflater.inflate(R.layout.fragment_main, container, false)
-            firstFavourite = true
+            Log.d("lifecycle","onCreateView")
             return rootView
         }
 
         override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
             super.onViewCreated(view, savedInstanceState)
-
-            if (arguments?.get(ARG_SECTION_NUMBER) == 1) {
-                this.loadNew()
-            } else {
-                loadFavourite()
-                Log.d("change_tab", "fav")
-            }
-
-        }
-
-        companion object {
-            /**
-             * The fragment argument representing the section number for this
-             * fragment.
-             */
-            private val ARG_SECTION_NUMBER = "section_number"
-
-            /**
-             * Returns a new instance of this fragment for the given section
-             * number.
-             */
-            fun newInstance(sectionNumber: Int): PlaceholderFragment {
-                val fragment = PlaceholderFragment()
-                val args = Bundle()
-                args.putInt(ARG_SECTION_NUMBER, sectionNumber)
-                fragment.arguments = args
-                return fragment
-            }
-        }
-
-        fun loadFavourite(): Unit {
-            var girlList = ArrayList<Girl>()
-            lateinit var adapter: GalleryGridViewAdapter
-            girlList = (activity!!.application as MyApplication).getLocalStorage()
-            if (firstFavourite) {
-                adapter = GalleryGridViewAdapter(context!!, girlList, object : OnDeleteListener {
-                    override fun onDeleteListener(postition: Int) {
-                        girlList.removeAt(postition)
-                        (activity!!.application as MyApplication).removeLocalStorage(postition)
-                        Toast.makeText(activity, "Delete From Your Favourite", Toast.LENGTH_LONG).show()
-                    }
-                })
-                galleryHolder.adapter = adapter
-                firstFavourite = false
-            }
-            adapter.notifyDataSetChanged()
-            Log.d("local_storage", girlList.size.toString())
+            this.loadNew()
+            Log.d("lifecycle","onViewCreated")
         }
 
         fun loadNew() {
             var galleryAdapter = GalleryGridViewAdapter(context!!, girls)
             galleryHolder.adapter = galleryAdapter
             galleryHolder.setOnItemClickListener { parent, view, position, id ->
-                val intent = Intent(activity, DetailActivity::class.java)
                 val girl = girls.get(position)
-                intent.putExtra("name", girl.name)
-                intent.putExtra("id", girl.id)
-                intent.putExtra("thumb", girl.thumb)
-                intent.putExtra("fb_url", girl.fb_url)
-                intent.putExtra("girl_gallery", girl.gallery)
-                startActivity(intent)
+                redirectToDetail(activity!!, girl)
             }
 
-            myRef.orderByKey().addChildEventListener(object : ChildEventListener {
+            myRef.orderByChild("time_stamp").addChildEventListener(object : ChildEventListener {
                 override fun onCancelled(p0: DatabaseError?) {
                     TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
                 }
@@ -171,4 +125,94 @@ class MainActivity : AppCompatActivity() {
             })
         }
     }
+
+    class HotFragment : Fragment() {
+        var database = FirebaseDatabase.getInstance()
+        var myRef = database.getReference("girl")
+        var girls = ArrayList<Girl>()
+        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+            val rootView = inflater.inflate(R.layout.fragment_hot, container, false)
+            return rootView
+        }
+
+        fun loadNew() {
+            var galleryAdapter = GalleryGridViewAdapter(context!!, girls)
+            hotGrid.adapter = galleryAdapter
+            hotGrid.setOnItemClickListener { parent, view, position, id ->
+                val girl = girls.get(position)
+                redirectToDetail(activity!!, girl)
+            }
+
+            myRef.orderByChild("time_stamp").addChildEventListener(object : ChildEventListener {
+                override fun onCancelled(p0: DatabaseError?) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+
+                override fun onChildMoved(p0: DataSnapshot?, p1: String?) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+
+                override fun onChildChanged(p0: DataSnapshot?, p1: String?) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+
+                override fun onChildAdded(p0: DataSnapshot?, p1: String?) {
+                    var girl = p0!!.getValue<Girl>(Girl::class.java)!!
+                    Log.d("lifecycle", girl.is_hot.toString())
+
+                    if (girl.is_hot) {
+                        girl.id = p0!!.key
+                        girls.add(girl)
+                        galleryAdapter.notifyDataSetChanged()
+                    }
+                }
+
+                override fun onChildRemoved(p0: DataSnapshot?) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+
+            })
+        }
+    }
+
+    class FavouriteFragment : Fragment() {
+        var favouriteGirls = ArrayList<Girl>()
+        lateinit var favouriteAdapter: GalleryGridViewAdapter
+        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+            val rootView = inflater.inflate(R.layout.fragment_favourite, container, false)
+            return rootView
+        }
+
+        override fun onResume() {
+            super.onResume()
+            loadFavourite()
+        }
+
+        fun loadFavourite(): Unit {
+            favouriteGirls = (activity!!.application as MyApplication).getLocalStorage()
+            favouriteAdapter = GalleryGridViewAdapter(context!!, favouriteGirls, object : OnDeleteListener {
+                override fun onDeleteListener(postition: Int) {
+                    favouriteGirls.removeAt(postition)
+                    (activity!!.application as MyApplication).removeLocalStorage(postition)
+                    Toast.makeText(activity, "Delete From Your Favourite", Toast.LENGTH_LONG).show()
+                }
+            })
+            favouriteGrid.adapter = favouriteAdapter
+            favouriteGrid.setOnItemClickListener { parent, view, position, id ->
+                val girl = favouriteGirls.get(position)
+                redirectToDetail(activity!!, girl)
+            }
+            favouriteAdapter.notifyDataSetChanged()
+        }
+    }
+}
+
+fun redirectToDetail(original: Activity, girl: Girl) {
+    val intent = Intent(original, DetailActivity::class.java)
+    intent.putExtra("name", girl.name)
+    intent.putExtra("id", girl.id)
+    intent.putExtra("thumb", girl.thumb)
+    intent.putExtra("fb_url", girl.fb_url)
+    intent.putExtra("girl_gallery", girl.gallery)
+    original.startActivity(intent)
 }
